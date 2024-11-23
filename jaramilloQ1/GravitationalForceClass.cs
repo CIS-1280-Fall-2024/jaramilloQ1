517d3cce22d12001e7a35a604d9e56865ea949f7
jaram <jaram@MSJARAMILLO>
2024-11-22 20:46:22 -0700

    Add project files.

---
+++ jaramilloQ1/GravitationalForceClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jaramilloQ1
+{
+    internal class GravitationalForceClass
+    {
+public class GravitationalForce
+    {
+        // Constant for the gravitational constant G
+        private const double G = 6.67408e-11;
+
+        // Properties for the names of the objects
+        public string Object1Name { get; set; }
+        public string Object2Name { get; set; }
+
+        // Properties for the masses of the objects
+        public double Mass1 { get; set; }
+        public double Mass2 { get; set; }
+
+        // Property for the distance between the objects
+        public double Radius { get; set; }
+
+        // Read-only property for the gravitational force
+        public double Force
+        {
+            get
+            {
+                return CalculateForce();
+            }
+        }
+
+        // Method to calculate the gravitational force
+        private double CalculateForce()
+        {
+            return G * (Mass1 * Mass2) / (Radius * Radius);
+        }
+
+        // Override ToString() method to return a formatted string
+        public override string ToString()
+        {
+            return $"The force between {Object1Name} and {Object2Name} is {Force:e2} N.";
+        }
+    }
+}
+}
---
+++ jaramilloQ1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static jaramilloQ1.GravitationalForceClass;
+namespace jaramilloQ1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GravitationalForce gf = new GravitationalForce
+            {
+                Object1Name = "Earth",
+                Object2Name = "Moon",
+                Mass1 = 5.972e24, // Mass of Earth in kg
+                Mass2 = 7.348e22, // Mass of Moon in kg
+                Radius = 3.844e8  // Distance between Earth and Moon in meters
+            };
+
+            Console.WriteLine(gf);
+        }
+    }
+}
